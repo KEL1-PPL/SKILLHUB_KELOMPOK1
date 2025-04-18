33ed17a6e62720d4b6f557d1130a68ecf5261996
Raffandafp <136280278+Raffandafp@users.noreply.github.com>
2025-04-18 16:16:46 +0700

    File Terbaru Commit

--- skill-hub/skill-hub/resources/views/ratings/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Rating Detail</h1>
-    <p>User: {{ $rating->user->name }}</p>
-    <p>Materi: {{ $rating->materi->title }}</p>
-    <p>Rating: {{ $rating->rating }}</p>
-@endsection
