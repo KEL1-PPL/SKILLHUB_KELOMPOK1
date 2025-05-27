f2653a6a8b4e9d841bc27f8648948aac92d6388c
Raffandafp <136280278+Raffandafp@users.noreply.github.com>
2025-05-27 19:05:57 +0700

    PPL2025-34

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Certificate extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'certificate_number',
+        'certificate_file',
+        'issued_at',
+    ];
+
+    // Relasi ke user (siswa)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi ke course (kursus)
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
